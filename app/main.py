057bc2086ca6125a3526de7840ad5dce97d611a0
mckenzieezell <aezell2003@gmail.com>
2026-07-27 14:14:51 -0400

    Refactor GPSController and remove logging setup

--- app/main.py
+++ app/main.py
@@ -1,23 +1,19 @@
 #!/usr/bin/env python3
 
 import requests
-import logging.handlers
-from pathlib import Path
 
 from litestar import Litestar, get
 from litestar.controller import Controller
-from litestar.datastructures import State
-from litestar.logging import LoggingConfig
 from litestar.static_files.config import StaticFilesConfig
 
 
 class GPSController(Controller):
 
     @get("/gps", sync_to_thread=True)
-    def get_gps(self, state: State) -> dict:
+    def get_gps(self) -> dict:
 
         # Ask MAVLink2Rest for the latest GLOBAL_POSITION_INT message
-        response = requests.get("http://host.docker.internal/mavlink2rest/mavlink/vehicles/1/components/1/messages/GLOBAL_POSITION_INT")
+        response = requests.get("http://blueos.local/mavlink2rest/mavlink/vehicles/1/components/1/messages/GLOBAL_POSITION_INT")
         response.raise_for_status()
 
         message = response.json()["message"]
@@ -26,37 +22,11 @@ class GPSController(Controller):
             "latitude": message["lat"] / 1e7,
             "longitude": message["lon"] / 1e7,
             "heading": message["hdg"] / 100,
-            "altitude": message["alt"] / 1000,
         }
 
-
-logging_config = LoggingConfig(
-    loggers={
-        __name__: dict(
-            level="INFO",
-            handlers=["queue_listener"],
-        )
-    },
-)
-
-log_dir = Path("/app/logs")
-log_dir.mkdir(parents=True, exist_ok=True)
-
-fh = logging.handlers.RotatingFileHandler(
-    log_dir / "lumber.log",
-    maxBytes=2**16,
-    backupCount=1,
-)
 
 app = Litestar(
     route_handlers=[GPSController],
-
-    state=State(
-        {
-            "mavlink_url": "http://host.docker.internal:6040/v1/mavlink"
-        }
-    ),
-
     static_files_config=[
         StaticFilesConfig(
             directories=["app/static"],
@@ -64,8 +34,6 @@ app = Litestar(
             html_mode=True,
         )
     ],
-
-    logging_config=logging_config,
 )
 
-app.logger.addHandler(fh)
+
